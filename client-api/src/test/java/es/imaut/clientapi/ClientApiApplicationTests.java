da1225b175be8d776e9d54155fbbe8d50fddd27a
Istvan Mosonyi <istvan.mosonyi@imautes.onmicrosoft.com>
2023-02-17 15:17:56 +0100

    Created empty client-api SB application

---
+++ client-api/src/test/java/es/imaut/clientapi/ClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.imaut.clientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
